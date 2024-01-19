b727e8207b5072c17e51000020fdd740c2765401
denezdas <dzunigaorb@educacion.navarra.es>
2024-01-19 12:08:55 +0100

    new file:   fich1.txt 	new file:   fich2.txt 	new file:   programa.py

---
+++ programa.py
@@ -0,0 +1,23 @@
+num = int(input("Dame un numero: "))
+
+uno = num*1
+dos = num*2
+tres = num*3
+cuatro = num*4
+cinco = num*5
+seis = num*6
+siete = num*7
+ocho = num*8
+nueve = num*9
+diez = num*10
+
+print(num"* 1 ="uno)
+print(num"* 2 ="dos)
+print(num"* 3 ="tres)
+print(num"* 4 ="cuatro)
+print(num"* 5 ="cinco)
+print(num"* 6 ="seis)
+print(num"* 7 ="siete)
+print(num"* 8 ="ocho)
+print(num"* 9 ="nueve)
+print(num"* 10 ="diez)
